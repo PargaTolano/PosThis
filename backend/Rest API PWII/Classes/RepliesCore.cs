4320c7e4fb93d48c447485f5e5e191ca3786bd3a
Esteban Martinez <teban.17mtz@gmail.com>
2021-04-19 14:42:54 -0500

    RepliesCore y RepliesController
    Falta checar el apartado de update

---
+++ backend/Rest API PWII/Classes/RepliesCore.cs
@@ -0,0 +1,147 @@
+﻿using Rest_API_PWII.Models;
+using Rest_API_PWII.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Rest_API_PWII.Classes
+{
+    public class RepliesCore
+    {
+        private PosThisDbContext db;
+
+        public RepliesCore(PosThisDbContext db)
+        {
+            this.db = db;
+        }
+
+        public ResponseApiError Validate(Reply reply)
+        {
+            if (reply.Post.Texto == null || reply.Post.Texto == "")
+                return new ResponseApiError{
+                    Code = 1,
+                    HttpStatusCode = (int)HttpStatusCode.BadRequest,
+                    Message = "Los datos de la respuesta no son validos, debe tener contenido de texto"
+                };
+
+            return null;
+        }
+
+        public ResponseApiError ValidateExists(Reply reply)
+        {
+            var res = (from r in db.Replies where r.ReplyID == reply.ReplyID select r).First();
+
+            if (res == null)
+                return new ResponseApiError{
+                    Code = 2,
+                    HttpStatusCode = (int)HttpStatusCode.NotFound,
+                    Message = "El reply no existe en la base de datos"
+                };
+
+            return null;
+        }
+
+        public ResponseApiError ValidateExists(int id)
+        {
+            var res = (from r in db.Replies where r.ReplyID == id select r).First();
+
+            if (res == null)
+                return new ResponseApiError { 
+                    Code = 2, 
+                    HttpStatusCode = (int)HttpStatusCode.NotFound, 
+                    Message = "El reply específico no existe en la base de datos" 
+                };
+
+            return null;
+        }
+
+        public ResponseApiError Create(Reply reply)
+        {
+            try
+            {
+                ResponseApiError err = Validate(reply);
+                if (err != null) 
+                    return err;
+
+                db.Replies.Add(reply);
+                db.SaveChanges();
+
+                return null;
+
+            }
+            catch (Exception ex)
+            {
+                return new ResponseApiError{
+                    Code = 3,
+                    HttpStatusCode = (int)HttpStatusCode.NotFound,
+                    Message = "Error interno del servidor"
+                };
+            }
+        }
+
+        public List<Reply> GetAll()
+        {
+            List<Reply> reply = (from r in db.Replies select r).ToList();
+
+            return reply;
+        }
+
+        public Reply GetOne(int id)
+        {
+            return db.Replies.First(p => p.PostID == id);
+        }
+
+        public ResponseApiError Update(int id, Reply reply)
+        {
+            try
+            {
+                ResponseApiError err = Validate(reply);
+                if (err != null)
+                    return err;
+
+                err = ValidateExists(id);
+                if (err != null)
+                    return err;
+
+                Reply replyDb = db.Replies.First(r => r.ReplyID == id); //Aqui no le puse toStrin por que compara int
+
+                //FALTA AQUI
+
+                //replyDb.Post.Texto = reply.Post.Texto != null ? reply.Post.Texto : replyDb.Post.Texto;
+
+                db.SaveChanges();
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public ResponseApiError Delete(int id)
+        {
+            try
+            {
+                ResponseApiError err = ValidateExists(id);
+                if (err != null)
+                    return err;
+
+                Reply replyDb = db.Replies.First(r => r.ReplyID == id);
+
+                db.Replies.Remove(replyDb);
+
+                db.SaveChanges();
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
---
+++ backend/Rest API PWII/Controllers/RepliesController.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rest_API_PWII.Classes;
+using Rest_API_PWII.Models;
+using Rest_API_PWII.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Rest_API_PWII.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class RepliesController : ControllerBase
+    {
+        private PosThisDbContext db;
+
+        RepliesController(PosThisDbContext db)
+        {
+            this.db = db;
+        }
+
+        // GET: api/<RepliesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                RepliesCore replyCore = new RepliesCore(db);
+                List<Reply> replies = replyCore.GetAll();
+
+                return Ok(
+                    new ResponseApiSuccess
+                    {
+                        Code = 1,
+                        Data = replies,
+                        Message = "Respuestas obtenidas exitosamente"
+                    });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(
+                    (int)HttpStatusCode.InternalServerError,
+                    new ResponseApiError
+                    {
+                        Code = 3,
+                        HttpStatusCode = (int)HttpStatusCode.InternalServerError,
+                        Message = ex.Message
+                    });
+            }
+        }
+
+        // GET api/<RepliesController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                RepliesCore replyCore = new RepliesCore(db);
+                Reply replies = replyCore.GetOne(id);
+
+                return Ok(
+                    new ResponseApiSuccess
+                    {
+                        Code = 1,
+                        Data = replies,
+                        Message = "Respuestas Obtenidas Exitosamente"
+                    });
+              
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(
+                   (int)HttpStatusCode.InternalServerError,
+                   new ResponseApiError
+                   {
+                       Code = 3,
+                       HttpStatusCode = (int)HttpStatusCode.InternalServerError,
+                       Message = ex.Message
+
+                   }) ;
+            }
+        }
+
+        // POST api/<RepliesController> //CREATE
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Reply reply)
+        {
+            try
+            {
+                RepliesCore repliesCore = new RepliesCore(db);
+                ResponseApiError err = repliesCore.Create(reply);
+                if (err != null)
+                    return StatusCode(err.HttpStatusCode, err);
+
+                return Ok(
+                    new ResponseApiSuccess
+                    {
+                        Code = 1,
+                        Data = reply,
+                        Message = "Respuesta creada exitosamente"
+                    });
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(
+                    (int)HttpStatusCode.InternalServerError,
+                    new ResponseApiError
+                    {
+                        Code= 3,
+                        HttpStatusCode = (int) HttpStatusCode.InternalServerError,
+                        Message = ex.Message
+                    });
+            }
+        }
+
+        // PUT api/<RepliesController>/5 
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            //Falta
+
+        }
+
+        // DELETE api/<RepliesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                RepliesCore repliesCore = new RepliesCore(db);
+                ResponseApiError err = repliesCore.Delete(id);
+                if (err != null)
+                    return StatusCode(err.HttpStatusCode, err);
+
+                return Ok(
+                    new ResponseApiSuccess
+                    {
+                        Code = 1,
+                        Data = "Success",
+                        Message = "Respuesta eliminada exitosamente"
+                    });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(
+                    (int)HttpStatusCode.InternalServerError,
+                    new ResponseApiError
+                    {
+                        Code = 3,
+                        HttpStatusCode = (int)HttpStatusCode.InternalServerError,
+                        Message = ex.Message
+                    });
+            }
+        }
+    }
+}
