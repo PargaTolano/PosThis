bc42503fa10f0f5aed1ea2db781d3f175fefce46
Valdemar Botello Jasso <valdemar33@hotmail.com>
2021-04-19 18:46:16 -0500

    Creación de LikesCore y LikesController

--- backend/Rest API PWII/Controllers/LikesController.cs
+++ backend/Rest API PWII/Controllers/LikesController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Rest_API_PWII.Classes;
 using Rest_API_PWII.Models;
+using Rest_API_PWII.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -14,19 +17,42 @@ namespace Rest_API_PWII.Controllers
     public class LikesController : ControllerBase
     {
         private PosThisDbContext db;
+        private LikesCore likesCore;
         public LikesController(PosThisDbContext db)
         {
             this.db = db;
         }
 
-        // GET: api/<LikesController1>
+        // GET: api/<LikesController>
         [HttpGet]
-        public IEnumerable<Likes> Get()
+        public IActionResult GetLikes()
         {
-            List<Likes> likes = db.Likes.ToList();
-            return likes;
+            likesCore = new LikesCore(db);
+            List<Likes> likes = likesCore.GetLikes();
+            return Ok(likes);
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody]Likes likes)
+        {
+            try 
+            {        
+                likesCore = new LikesCore(db);
+                ResponseApiError responseApiError = likesCore.Create(likes);
+
+                if(responseApiError != null)
+                {
+                    return StatusCode(responseApiError.HttpStatusCode,responseApiError);
+                }
+                return Ok(new ResponseApiSuccess { Code = 1, Message = "Estudiante creado exitoxamente"});
+            }
+            catch(Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new ResponseApiError { Code=1001, Message = ex.Message});
+            }
+    
         }
 
-       
+
+
     }
 }
