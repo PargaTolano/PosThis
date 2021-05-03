17a4176c453c6f0a1af2a7714098804f5d49daf8
Esteban Martinez <teban.17mtz@gmail.com>
2021-05-03 18:22:21 -0500

    Login
    Login terminado para la página, falta checar el modal de registro

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,26 +1,131 @@
-import React from 'react';
-import Logo from 'assets/placeholder.png';
+import React from "react";
+import Logo from "assets/placeholder.png";
 
-import { getUsers } from 'API/User.API';
-import useRequestLoadOnMount from 'hooks/useRequestLoadOnMount';
+import {
+  Grid,
+  Container,
+  TextField,
+  FormControlLabel,
+  Button,
+  Link,
+  CssBaseline,
+  Avatar,
+  Paper,
+  Checkbox,
+  Typography,
+} from "@material-ui/core";
 
-const Login = ( props ) => {
+import { makeStyles } from "@material-ui/core/styles";
+import PersonPinIcon from "@material-ui/icons/PersonPin";
 
-    const { ready, response } = useRequestLoadOnMount( getUsers );
-    
-    return (
-        <div className="login">
-            <header></header>
-            <aside><p>A la derecha apareceran usuarios ;)</p></aside>
-            <main>
-                <div>
-                    {
-                        ready && response.data.map( u => <h2 key={u.Id}>{u.userName}</h2>)
-                    }
-                </div>
-            </main>  
+import { getUsers } from "API/User.API";
+import useRequestLoadOnMount from "hooks/useRequestLoadOnMount";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage:
+      "url(/img/backgroundPT.png)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.dark,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const Login = (props) => {
+  const { ready, response } = useRequestLoadOnMount(getUsers);
+  const classes = useStyles();
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <PersonPinIcon />
+          </Avatar>
+
+          <Typography component="h1" variant="h5">
+            <strong>Inicia Sesión</strong>
+          </Typography>
+
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Correo electrónico"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Contraseña"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Recordar contraseña"
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  ¿Olvidaste tu contraseña?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Regístrate aquí"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
         </div>
-    );
-}
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Login;
