41134411a2e11cef67fb4d87553dcd9fcb91d028
PargaTolano <parga.jose@outlook.com>
2021-05-03 15:03:10 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Feed from 'components/Feed';
 import Login from 'components/Login';
 
+import useCheckAuth from 'hooks/useCheckAuth';
+
 import logo from './logo.svg';
 import './App.css';
 
